bd1149323ca8525ae76fc1a0729d627a38830a0f
Charles Grisar <charlesgrisar@Charless-iMac-3.local>
2019-06-07 08:54:22 +0200

    lightbox

--- site/levipe-vue/src/components/miscellaneous/lightbox.vue
+++ site/levipe-vue/src/components/miscellaneous/lightbox.vue
@@ -3,27 +3,11 @@
       <a href="#" @click.prevent="show" class="w-full">
           <img :src="thumbnail" alt="{{ alt }}" class="w-1/2 md:w-1/3 lg:w-1/4 mt-8 shadow-md rounded-full" />
       </a>
-      <div class="lightbox fixed pin z-50 flex justify-center items-center" v-if="visible" @click="hide">
+      <div class="lightbox fixed pin z-50 flex justify-center items-center bg-black opacity-75" v-if="visible" @click="hide">
           <div class="fixed pin-r pin-t text-white cursor-pointer text-4xl p-1 mr-2" @click.stop="hide">&times;</div>
           <div class="flex">
-              <div class="cursor-pointer self-center px-8"
-                   @click.stop="prev"
-                   :class="{'invisible': ! hasPrev()}">
-                  <svg class="pointer-events-none" fill="#fff" height="48" viewBox="0 0 24 24" width="48" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z"/>
-                      <path d="M0-.5h24v24H0z" fill="none"/>
-                  </svg>
-              </div>
               <div class="lightbox-image" @click.stop="">
-                  <img :src="images[index]">
-              </div>
-              <div class="cursor-pointer self-center px-8"
-                   @click.stop="next"
-                   :class="{'invisible': ! hasNext()}">
-                  <svg class="pointer-events-none" fill="#fff" height="48" viewBox="0 0 24 24" width="48" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"/>
-                      <path d="M0-.25h24v24H0z" fill="none"/>
-                  </svg>
+                  <img :src="thumbnail" />
               </div>
           </div>
       </div>
