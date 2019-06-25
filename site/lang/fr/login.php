afbdebe9c352e32a5e67b5db05e40162c73d7252
Charles Grisar <charlesgrisar@Charless-iMac-3.local>
2019-06-25 10:28:33 +0200

    medium login

--- site/lang/fr/login.php
+++ site/lang/fr/login.php
@@ -15,7 +15,7 @@ return [
     'promotion_message' => 'Bénéficiez des promotions que nous organisons à mesure des saisons!',
     'transport_header' => 'Livraison Gratuite',
     'transport_message' => 'A partir de 85 EUR commandés, nous prenons la livraison en charge!',
-    'assurance_header' => 'Satisfait our Remboursé',
+    'assurance_header' => 'Satisfait ou Remboursé',
     'assurance_message' => 'Vous bénéficiez de 14 jours de réflexion durant lesquels nous reprenons et remboursons toute bouteille commandée non ouverte',
 
 ];
