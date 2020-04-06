9a0a18c77b42ffa51c21f0a4b81627227f694d80
Charles Grisar <charlesgrisar@charless-imac-3.home>
2020-04-06 17:44:45 +0200

    information

--- site/lang/fr/collection.php
+++ site/lang/fr/collection.php
@@ -5,11 +5,12 @@ return [
     'estate' => ' Domaine viticole',
     'grape' => ' Cépage',
     'region' => ' Région',
+    'article' => ' Information',
     'delivery' => ' T & D',
     'estate_pb' => 'Domaines',
     'wines_pb' => 'Vins',
-    'articles_pb' => 'News',
     'region_pb' => 'Régions',
     'grape_pb' => 'Cépages',
+    'article_pb' => ' Information',
     'delivery_pb' => 'T & D'
 ];
--- site/lang/nl/collection.php
+++ site/lang/nl/collection.php
@@ -5,11 +5,12 @@ return [
     'estate' => ' Wijndomein',
     'grape' => ' Druivensoort',
     'region' => ' Streek of streeknaam',
+    'article' => ' Informatie',
     'delivery' => ' T & D',
     'estate_pb' => 'Domeinen',
     'wines_pb' => 'Wijnen',
-    'articles_pb' => 'Nieuws',
     'region_pb' => 'Streken',
     'grape_pb' => 'Druiven',
+    'article_pb' => ' Informatie',
     'delivery_pb' => 'T & D'
 ];
